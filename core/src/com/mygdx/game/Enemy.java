7df0fb074c16fc081612e9ae1f264cf9bb38dbb7
elomir <elomir.fj@live.com>
2021-12-06 00:39:52 -0300

    added enemies and game score

---
+++ core/src/com/mygdx/game/Enemy.java
@@ -0,0 +1,33 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Enemy extends Actor {
+
+    static final int RIGHT = 0;
+    static final int LEFT = 1;
+    int direction;
+
+    Enemy(Texture texture, MyGdxGame game) {
+        super(game.random.nextInt((int)game.w - texture.getWidth()),
+                game.h + 100, texture, game);
+        direction = game.random.nextInt(2);
+    }
+
+    @Override
+    void execute(){
+        sprite.translateY(-5);
+        if (direction == RIGHT) {
+            sprite.translateX(5);
+            if (sprite.getX() >= game.w - sprite.getWidth()) {
+                direction = LEFT;
+            }
+        } else {
+            sprite.translateX(-5);
+            if (sprite.getX() <= 0) {
+                direction = RIGHT;
+            }
+        }
+    }
+
+}
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,17 +2,31 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture textureBackground;
 	Texture textureShip;
-	float w, h;
+	Texture textureEnemy;
+	Texture textureGameOver;
 	Background background;
-	Actor avatar;
+	List<Actor> actors = new ArrayList<>();
+	Random random = new Random();
+	Music mainTheme;
+	BitmapFont font;
+	float w, h;
+	int enemyCount = 0;
+	int enemyMax = 50;
+	int score = 0;
 	
 	@Override
 	public void create () {
@@ -20,13 +34,39 @@ public class MyGdxGame extends ApplicationAdapter {
 		h = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
 		textureBackground = new Texture("sea_sprite.png");
-		background = new Background(textureBackground, this);
 		textureShip = new Texture("USA1.png");
-		avatar = new Actor(w / 2f - textureShip.getWidth() / 2f, 10, textureShip, this);
+		textureEnemy = new Texture("JPN3.png");
+		textureGameOver = new Texture("game_over_screen.png");
+		background = new Background(textureBackground, this);
+		mainTheme = Gdx.audio.newMusic(Gdx.files.internal("main_theme.ogg"));
+		actors.add(new Actor(w / 2f - textureShip.getWidth() / 2f, 10, textureShip, this));
+		mainTheme.setLooping(true);
+		mainTheme.play();
+		font = new BitmapFont(
+			Gdx.files.internal("verdana.fnt"), Gdx.files.internal("verdana.png"), false
+		);
+	}
+
+	void generateEnemies() {
+		enemyCount++;
+		if (enemyCount >= enemyMax){
+			actors.add(new Enemy(textureEnemy, this));
+			enemyCount = 0;
+			enemyMax = 30 + random.nextInt(50);
+		}
+	}
+
+	void cleanEnemies() {
+		List<Actor> aux = actors;
+		actors = new ArrayList<>();
+		for (Actor a : aux) if (!a.dead) actors.add(a);
 	}
 
 	public void run() {
 		background.run();
+		for (Actor a : actors) a.run();
+		generateEnemies();
+		cleanEnemies();
 	}
 
 	@Override
@@ -35,7 +75,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		background.draw();
-		avatar.draw();
+		for (Actor a : actors) a.draw();
+		font.draw(batch, "SCORE: " + score, 1, h);
 		batch.end();
 	}
 	
@@ -44,5 +85,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.dispose();
 		textureBackground.dispose();
 		textureShip.dispose();
+		textureEnemy.dispose();
+		textureGameOver.dispose();
 	}
 }
